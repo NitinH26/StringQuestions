ccfb5684fa36005f2d42e4adfaa23de3da9ebe23
Nitin <nitinhude26@gmail.com>
2023-05-12 03:42:34 +0530

    firstcommit

---
+++ stringPrograms/src/Using_chatAt/Class1.java
@@ -0,0 +1,17 @@
+package Using_chatAt;
+
+public class Class1 
+{
+	//Counting Frequency of a character in a String by Using the charAt() Method
+	  public static void main(String[] args) {  
+	        String str = "Welcome to Javatpoint portal";  
+	        int count = 0;  
+	        for (int i=0; i<=str.length()-1; i++) {  
+	            if(str.charAt(i) == 't') {  
+	                count++;  
+	            }  
+	        }  
+	        System.out.println("Frequency of t is: "+count);  
+	    }  
+
+}
